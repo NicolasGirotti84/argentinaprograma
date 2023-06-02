c404c156e574a9ef68f7986bbc942ef3a24fb919
Nicolas Girotti <90194206+NicolasGirotti84@users.noreply.github.com>
2023-06-01 21:15:41 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+let base1 = 5;
+let altura1 = 3;
+let base2 = 12.5;
+let altura2 = 5.3;
+areaRectagulo1 = base1 * altura1;
+areaRectagulo2 = base2 * altura2;
+console.log(areaRectagulo1);
+console.log(areaRectagulo2);
---
+++ practico0/promedio3.js
@@ -0,0 +1,9 @@
+let suma_positivos = 5 + 7 + 9;
+let suma_negativos = -2 + -3 + -8;
+let suma_decimales = -1.2 + 5.3 + -3.3;
+let promedioPositivos = suma_positivos / 3;
+let promedioNegativos = suma_negativos / 3;
+let promedioDecimales = suma_decimales / 3;
+console.log(promedioPositivos);
+console.log(promedioNegativos);
+console.log(promedioDecimales);
