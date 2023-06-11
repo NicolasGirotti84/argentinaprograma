60cf8c551f5d58214951d160c6b234156ddbd274
Nicolas Girotti <90194206+NicolasGirotti84@users.noreply.github.com>
2023-06-11 01:39:38 -0300

    Agrego ejercicio 5

---
+++ practico1/frutas.js
@@ -0,0 +1,7 @@
+let frutas =['Banana','Manzana','Pera','Durazno','Naranja'];
+
+let ultima = frutas[4];
+let primera = frutas[0];
+
+console.log("La ultima fruta agregada fue: "+ultima);
+console.log("La primera fruta agregada fue: "+primera);
